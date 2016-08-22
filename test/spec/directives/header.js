fe4ea1a5558e6e85c9a39df4c4d4d594bb03b36f
Mohd Raza <mohd.raza@allies.local>
2016-08-22 19:25:20 +0530

    Directive test cases written

--- test/spec/directives/header.js
+++ test/spec/directives/header.js
@@ -8,13 +8,27 @@ describe('Directive: header', function () {
   var element,
     scope;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile, $templateCache) {
     scope = $rootScope.$new();
+    compile = $compile;
+
+    $templateCache.put('views/common/header.html',
+    '<div class="navbar-header">' +
+      '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'+
+        '<i class="fa fa-align-justify top-menu-icon" aria-hidden="true"></i>'+
+      '</button>'+
+    '</div>');
+
+    scope.template = {
+     url: 'views/common/header.html'
+    };
+    element = compile(angular.element('<main-header></main-header>'));
+
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<header></header>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the header directive');
-  }));
+  // it('should make hidden element visible', inject(function ($compile) {
+  //   element = angular.element('<header></header>');
+  //   element = $compile(element)(scope);
+  //   expect(element.text()).toBe('this is the header directive');
+  // }));
 });
