f8186ede22a0dd96dc45dec58475c94c4707e7b6
Abdul Qabiz <abdul.qabiz@gmail.com>
2016-08-25 04:53:04 +0530

    Update Build Tools

--- tools/build-deploy.sh
+++ tools/build-deploy.sh
@@ -4,23 +4,20 @@ grunt test
 grunt build
 
 cd ./tools/
-rm -rf ./deploy/dist/*
-mkdir -p ./deploy/dist
-cp -a ../dist/ ./deploy/dist/
 
-cp Dockerfile.deploy.template ./deploy/dist/Dockerfile
-cp Caddyfile.template ./deploy/dist/Caddyfile
-
-DOCKER_HOST=tcp://docker.appfactory.in:2376 docker build -t --no-cache=true docker.appfactory.in/coinomia-frontend:latest deploy/dist
+cp -a ../dist ./dist
+DOCKER_HOST=tcp://docker.appfactory.in:2376 docker build --no-cache=true -t docker.appfactory.in/coinomia-frontend:latest -f Dockerfile.image
 DOCKER_HOST=tcp://docker.appfactory.in:2376 docker push docker.appfactory.in/coinomia-frontend:latest
 
-rm -rf ./deploy/dist
+rm -rf ./dist
 
-cp Dockerfile.template ./deploy/Dockerfile
+cp Dockerfile.run ./deploy/Dockerfile
 
 cd ./deploy/
 git init .
 git add .
+git config --local user.email "bot@allies.co.in"
+git config --local user.name "Build Bot"
 git commit -m "Coinomia Build"
 git remote add dokku dokku@apps.appfactory.in:coinomia
 git push dokku master --force
