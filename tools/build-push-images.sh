71f2deccdb91cede2f9d16f1b73c53d6baa89047
Abdul Qabiz <abdul.qabiz@gmail.com>
2016-08-26 00:58:16 +0530

    Fix drone issues

--- tools/build-push-images.sh
+++ tools/build-push-images.sh
@@ -1,16 +1,16 @@
-echo "Building coinomia-frontend:latest images"
-cp -a ../dist ./dist
-cp Dockerfile.image ./dist/Dockerfile
-DOCKER_HOST=tcp://docker.appfactory.in:2376 docker build --no-cache -t docker.appfactory.in/coinomia-frontend:latest dist
-DOCKER_HOST=tcp://docker.appfactory.in:2376 docker push docker.appfactory.in/coinomia-frontend:latest
-echo "docker.appfactory.in/coinomia-frontend:latest pushed to registry"
-rm -rf ./dist
+#echo "Building coinomia-frontend:latest images"
+#cp -a ../dist ./dist
+#cp Dockerfile.image ./dist/Dockerfile
+#DOCKER_HOST=tcp://docker.appfactory.in:2376 docker build --no-cache -t docker.appfactory.in/coinomia-frontend:latest dist
+#DOCKER_HOST=tcp://docker.appfactory.in:2376 docker push docker.appfactory.in/coinomia-frontend:latest
+#echo "docker.appfactory.in/coinomia-frontend:latest pushed to registry"
+#rm -rf ./dist
 
-echo "Building image for Dokku"
+#echo "Building image for Dokku"
 
-cp Dockerfile.run ./deploy/Dockerfile
 
-cd ./deploy/
+rm -rf $DRONE_DIR/deploy/.git
+cd $DRONE_DIR/deploy
 git init .
 git add .
 git config --local user.email "bot@allies.co.in"
@@ -20,5 +20,5 @@ git remote add dokku dokku@apps.appfactory.in:coinomia
 git push dokku master --force
 echo "App pushed to Dokku"
 
-rm -rf ./.git
-rm Dockerfile
+rm -rf $DRONE_DIR/deploy/.git
+
