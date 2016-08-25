df99318fdad301440c50d8d6ef81a714a4fbc464
Abdul Qabiz <abdul.qabiz@gmail.com>
2016-08-26 03:32:25 +0530

    Fix deploy block to use ssh and exectute some commands remotely
    It seems, drone:0.4 has removed bash plugin from deploy block, and
    third-party plugins didn't work. I am not resorting to execute a few
    scripts on remote host via ssh to deploy images to Dokku.

---
+++ tools/deploy-staging-stable.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DEPLOY_DIR=/tmp/deploy_$DRONE_BUILD_NUMBER
+mkdir -p /tmp/deploy_$DRONE_BUILD_NUMBER
+echo "FROM docker.appfactory.in/coinomia-frontend:stable" > $DEPLOY_DIR/Dockerfile
+cd $DEPLOY_DIR
+git init .
+git config --local user.email "bot@allies.co.in"
+git config --local user.name "Build Bot"
+git commit -m "Coinomia Stable Build - $DRONE_BRANCH - $DRONE_COMMIT - $DRONE_BUILD_NUMBER"
+git remote add dokku dokku@apps.appfactory.in:coinomia-stable
+git push dokku master --force
+echo "$DRONE_BRANCH pushed to Dokku"
+cd /tmp
+rm -rf $DEPLOY_DIR
+
+
