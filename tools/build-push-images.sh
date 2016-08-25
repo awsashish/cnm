cc786e96cca31bb95fe681326d9f261f23ee3aa6
Abdul Qabiz <abdul.qabiz@gmail.com>
2016-08-25 21:25:15 +0530

    Update build tools

--- tools/build-push-images.sh
+++ tools/build-push-images.sh
@@ -1,15 +1,11 @@
-cd ../
-npm install && bower install
-grunt test
-grunt build
-
-cd ./tools/
-
+echo "Building coinomia-frontend:latest images"
 cp -a ../dist ./dist
 DOCKER_HOST=tcp://docker.appfactory.in:2376 docker build --no-cache=true -t docker.appfactory.in/coinomia-frontend:latest -f Dockerfile.image
 DOCKER_HOST=tcp://docker.appfactory.in:2376 docker push docker.appfactory.in/coinomia-frontend:latest
-
+echo "docker.appfactory.in/coinomia-frontend:latest pushed to registry"
 rm -rf ./dist
+
+echo "Building image for Dokku"
 
 cp Dockerfile.run ./deploy/Dockerfile
 
@@ -21,6 +17,7 @@ git config --local user.name "Build Bot"
 git commit -m "Coinomia Build"
 git remote add dokku dokku@apps.appfactory.in:coinomia
 git push dokku master --force
+echo "App pushed to Dokku"
 
 rm -rf ./.git
 rm Dockerfile
