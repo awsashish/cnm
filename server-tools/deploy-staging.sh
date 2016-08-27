85c2db7a22efd39fcc2b9c39c19afa6db52a32e9
Abdul Qabiz <abdul.qabiz@gmail.com>
2016-08-27 17:59:08 +0530

    Deploy an app for each branch to Dokku

--- server-tools/deploy-staging.sh
+++ server-tools/deploy-staging.sh
@@ -5,10 +5,10 @@ BRANCH=$2
 COMMIT=$3
 DEPLOY_DIR=/tmp/deploy_$DEPLOY_TYPE
 
-DOCKER_IMAGE="docker.appfactory.in/coinomia-frontend:$DEPLOY_TYPE"
+DOCKER_IMAGE="docker.appfactory.in/coinomia-frontend:$BRANCH"
 
 #Make sure Dokku Host has latest image from registry
-dokku registry:pull coinomia-$DEPLOY_TYPE $DOCKER_IMAGE
+dokku registry:pull coinomia-$BRANCH $DOCKER_IMAGE
 
 mkdir -p $DEPLOY_DIR
 echo "FROM $DOCKER_IMAGE" > $DEPLOY_DIR/Dockerfile
@@ -18,7 +18,7 @@ git add Dockerfile
 git config --local user.email "bot@allies.co.in"
 git config --local user.name "Build Bot"
 git commit -m "Coinomia $DEPLOY_TYPE - $BRANCH - $COMMIT"
-git remote add dokku dokku@apps.appfactory.in:coinomia-$DEPLOY_TYPE
+git remote add dokku dokku@apps.appfactory.in:coinomia-$BRANCH
 git push dokku master --force
 echo "$DEPLOY_TYPE - $BRANCH (commit: $COMMIT)  pushed to Dokku"
 cd /tmp
