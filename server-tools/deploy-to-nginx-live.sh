d6b9c903a76496e812ebb47533f0b73e69f29383
Abdul Qabiz <abdul.qabiz@gmail.com>
2016-08-27 17:17:13 +0530

    Pass COMMIT ID to deployment scripts, and store it in text file on server

--- server-tools/deploy-to-nginx-live.sh
+++ server-tools/deploy-to-nginx-live.sh
@@ -1,7 +1,10 @@
+#!/bin/sh
 
 DRONE_JOB_NUMBER=$1
+COMMIT=$2
 NGINX_DIR="/usr/share/nginx"
 HTML_NEW_DIR=" /home/coinomia/deployments/live/dist_$DRONE_JOB_NUMBER"
+echo "commit: $COMMIT" > $HTML_NEW_DIR/commit.txt
 
 #sudo cp -a /home/coinomia/deployments/dist_$DRONE_JOB_NUMBER/ $HTML_NEW_DIR/ && \
 #     rm $NGINX_DIR/html && \
