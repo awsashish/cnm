07a23e4a53997f235d4856dcd395a3ccada93087
Mohammad Raza <mohd.raza@allies.co.in>
2016-09-03 17:57:19 +0530

    Test case written for user setting page

--- test/spec/controllers/setting.js
+++ test/spec/controllers/setting.js
@@ -12,6 +12,11 @@ describe('Controller: DashboardCtrl', function () {
       coinomiaServiceDeferred,
       scope;
 
+  var postData = {
+    'password': '123456a',
+    'confirmPassword': '123456a'
+  }
+
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, _$state_, _coinomiaService_, _$log_, _$q_) {
     $q = _$q_;
@@ -21,14 +26,29 @@ describe('Controller: DashboardCtrl', function () {
     coinomiaServiceDeferred = $q.defer();
 
     spyOn(coinomiaService, 'getUserInfo').and.returnValue(coinomiaServiceDeferred.promise);
+    spyOn(coinomiaService, 'changePassword').and.returnValue(coinomiaServiceDeferred.promise);
     SettingCtrl = $controller('SettingCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
+    scope.user = {
+      'NewPassword': '123456a',
+      'ConfirmPassword': '123456a'
+    };
   }));
 
   it('should be defined and call getUserInfo', function () {
     scope.getUserProfile();
     expect(coinomiaService.getUserInfo).toHaveBeenCalled();
+  });
+
+  it('should be define and check password mismatch', function() {
+    scope.confirmPass(scope.user);
+    expect(scope.confirmPassError).toEqual(false);
+  });
+
+  it('should be define and call change password service', function() {
+    scope.changeUserPasssword(scope.user);
+    expect(coinomiaService.changePassword).toHaveBeenCalledWith(scope.user);
   });
 });
