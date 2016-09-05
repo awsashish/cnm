b25d923b31f5e8dee426879549abeaaf1e998d75
Mohammad Raza <mohd.raza@allies.co.in>
2016-09-05 15:31:37 +0530

    Test cases written for footer directive

--- test/spec/directives/footer.js
+++ test/spec/directives/footer.js
@@ -8,13 +8,23 @@ describe('Directive: footer', function () {
   var element,
     scope;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile, $templateCache) {
     scope = $rootScope.$new();
+    $templateCache.put('views/common/footer.html',
+    '<footer style="margin-top:50px;">' +
+        '<div class="container">' +
+            '<p class="pull-right">copyright all right reserved 2016</p>' +
+        '</div>' +
+    '</footer>');
+
+    element = angular.element("<main-footer></main-footer>");
+    $compile(element)(scope);
+    scope.$digest();
+
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<footer></footer>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBeDefined();
-  }));
+  it('Should load template', function () {
+     expect(element.find('.container').length).toEqual(1); //Test if element has loaded template properly
+     expect(element.find('.pull-right').text()).toEqual("copyright all right reserved 2016");
+  });
 });
