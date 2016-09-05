20c7de922d0e5e07ee50f32ccd4528f1e041c74f
Mohammad Raza <mohd.raza@allies.co.in>
2016-09-05 18:16:12 +0530

    Test case written for navigation directive

--- test/spec/directives/navigation.js
+++ test/spec/directives/navigation.js
@@ -3,18 +3,29 @@
 describe('Directive: navigation', function () {
 
   // load the directive's module
-  beforeEach(module('coinomiaFrontendApp'));
+  beforeEach(module('coinomiaFrontendApp',function($controllerProvider) {
+    $controllerProvider.register('NavCtrl', function($scope) {
+        // Controller Mock
+    });
+  }));
+
+  beforeEach(module('views/common/navigation.html'));
 
   var element,
+    controller,
+    ctrl,
     scope;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile, _$controller_) {
     scope = $rootScope.$new();
+    element = angular.element("<navigation></navigation>");
+    $compile(element)(scope);
+    // ctrl = _$controller_('NavCtrl', {$scope: scope});
+    scope.$digest();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<navigation></navigation>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBeDefined();
-  }));
+  it('Should load template', function () {
+     expect(element.find('.leftpanel').length).toEqual(1);  //Test if element has loaded template properly
+     expect(element.find('.logout').attr('ng-click')).toEqual('logout()');
+  });
 });
