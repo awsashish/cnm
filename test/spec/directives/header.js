1d73d4662916830d0d826f4a98565d43857bbf9c
Mohammad Raza <mohd.raza@allies.co.in>
2016-09-07 17:06:09 +0530

    Test cases written for header directives

--- test/spec/directives/header.js
+++ test/spec/directives/header.js
@@ -4,6 +4,13 @@ describe('Directive: header', function () {
 
   // load the directive's module
   beforeEach(module('coinomiaFrontendApp'));
+  // load the directive's module
+  beforeEach(module('coinomiaFrontendApp',function($controllerProvider) {
+    $controllerProvider.register('HeaderCtrl', function($scope) {
+        // Controller Mock
+    });
+  }));
+
   beforeEach(module('views/common/header.html'))
 
   var element,
@@ -22,4 +29,8 @@ describe('Directive: header', function () {
      expect(element.find('.navbar-header').length).toEqual(1); //Test if element has loaded template properly
      expect(element.find('.top-menu-icon').attr('aria-hidden')).toEqual("true");
   });
+
+  it('should display welcome value', function() {
+    expect(element.find('.user-info').html()).toContain('Welcome');
+  })
 });
