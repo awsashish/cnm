a2ebf8c6ce924846a46ab454ff771c46a751fa91
Mohammad Raza <mohd.raza@allies.co.in>
2016-09-07 16:27:19 +0530

    Nav controller modified and header controller created to show user details in header

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coinomiaFrontendApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the coinomiaFrontendApp
+ */
+angular.module('coinomiaFrontendApp')
+  .controller('HeaderCtrl', function ($scope, $cookies, $state, $rootScope, $localStorage, $timeout, coinomiaService) {
+
+      $rootScope.tokenRequestProgress = false;
+
+      $rootScope.$on('getRefreshToken', function() {
+
+        if($localStorage.refresh_token && !$cookies.get('token') && !$rootScope.tokenRequestProgress) {
+          $rootScope.tokenRequestProgress = true;
+          var refreshTokenParams = {
+            'grant_type': 'refresh_token',
+            'refresh_token': $localStorage.refresh_token
+          }
+
+          coinomiaService.getRefreshToken(refreshTokenParams).then(function(res) {
+            var data = res.data;
+            if(res.status === 200) {
+              $localStorage.$reset();
+              $cookies.put('token', data.access_token, {expires:moment().second(data.expires_in).toISOString()});
+              $localStorage.$default({token: data.access_token, expires:moment().second(data.expires_in).toISOString(), refresh_token:data.refresh_token});
+            }else{
+              $scope.logout();
+            }
+            $rootScope.tokenRequestProgress = false;
+          });
+        }else if(!$localStorage.refresh_token){
+          $scope.logout();
+        }
+      });
+
+    //Get User Info
+    $scope.getUserDetails = function() {
+      coinomiaService.getUserInfo().then(function(res) {
+        var data = res.data;
+        if(res.status === 200){
+          $rootScope.name = data.name;
+          $scope.country = data.Country;
+          $scope.name = $rootScope.name;
+        }
+      });
+    }
+
+    $scope.getUserDetails();
+  });
--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -9,47 +9,7 @@
  */
 angular.module('coinomiaFrontendApp')
 .controller('NavCtrl', function ($scope, $cookies, $state, $rootScope, $localStorage, $timeout, coinomiaService) {
-
-  $rootScope.tokenRequestProgress = false;
-
-  $rootScope.$on('getRefreshToken', function() {
-
-    if($localStorage.refresh_token && !$cookies.get('token') && !$rootScope.tokenRequestProgress) {
-      $rootScope.tokenRequestProgress = true;
-      var refreshTokenParams = {
-        'grant_type': 'refresh_token',
-        'refresh_token': $localStorage.refresh_token
-      }
-
-      coinomiaService.getRefreshToken(refreshTokenParams).then(function(res) {
-        var data = res.data;
-        if(res.status === 200) {
-          $localStorage.$reset();
-          $cookies.put('token', data.access_token, {expires:moment().second(data.expires_in).toISOString()});
-          $localStorage.$default({token: data.access_token, expires:moment().second(data.expires_in).toISOString(), refresh_token:data.refresh_token});
-        }else{
-          $scope.logout();
-        }
-        $rootScope.tokenRequestProgress = false;
-      });
-    }else if(!$localStorage.refresh_token){
-      $scope.logout();
-    }
-  });
-
-  //Get User Info
-  $scope.getUserDetails = function() {
-    coinomiaService.getUserInfo().then(function(res) {
-      var data = res.data;
-      if(res.status === 200){
-        $rootScope.name = data.name;
-        $scope.name = $rootScope.name;
-      }
-    });
-  }
-
-  $scope.getUserDetails();
-
+  
  //  Logged out User
  $scope.logout = function() {
    if($localStorage.token || $cookies.get('token')) {
