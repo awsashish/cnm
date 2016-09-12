a65f89ac3abd30ca934ebd5471c6e93faa743f03
Mohammad Raza <mohd.raza@allies.co.in>
2016-09-12 15:58:53 +0530

    Coming soon pages created

---
+++ test/spec/directives/comingsoon.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('Directive: comingSoon', function () {
+
+  // load the directive's module
+  beforeEach(module('coinomiaFrontendApp'));
+  beforeEach(module('views/directiveTemplates/comingSoon.html'));
+
+  var element,
+    controller,
+    rootScope,
+    ctrl,
+    dscope,
+    scope;
+
+  beforeEach(inject(function ($rootScope, $compile, _$controller_) {
+    scope = $rootScope.$new();
+    element = angular.element("<coming-soon text-value='some-text' />");
+    $compile(element)(scope);
+    scope.$digest();
+    dscope = element.isolateScope();
+  }));
+
+  it('Should load template', function () {
+     expect(element.find('.coming-soon-wrapper h4').html()).toContain('will be coming soon');
+  });
+
+  it('should have the scope values', function() {
+    expect(scope).toBeDefined();
+    expect(dscope.textValue).toBeDefined();
+  });
+});
