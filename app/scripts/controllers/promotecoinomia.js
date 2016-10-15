2a3db71423c5ac7b6523a02cc320701eda23a3b1
Mohammad Raza <mohd.raza@allies.co.in>
2016-10-15 17:06:10 +0530

    Campaign Report
    - Campaing tab and it's content added in promote coinomia page.
    - Logged in user's referral links showing in the pop when click on Get
      Referral Link button.

--- app/scripts/controllers/promotecoinomia.js
+++ app/scripts/controllers/promotecoinomia.js
@@ -25,18 +25,35 @@ angular.module('coinomiaFrontendApp')
 
     // Get Banners
     coinomiaService.getBanners()
-      .then(function(res) {
-        if(res.status === 200) {
-          var data = res.data;
-          $scope.totalBanners = data.total;
-          var bannerData = data.rows;
-          $scope.banners = [];
-          bannerData.forEach(function(banner) {
-            banner.bannerimage = banner.bannerimage.replace('~', '');
-            banner.showCode = false;
-            $scope.banners.push(banner);
+    .then(function(res) {
+      if(res.status === 200) {
+        var data = res.data;
+        $scope.totalBanners = data.total;
+        var bannerData = data.rows;
+        $scope.banners = [];
+        bannerData.forEach(function(banner) {
+          banner.bannerimage = banner.bannerimage.replace('~', '');
+          banner.showCode = false;
+          $scope.banners.push(banner);
+        });
+      }
+    });
+
+
+    $scope.getReferralLinks = function() {
+      //  Get Landing pages and referral links
+      coinomiaService.getLandingPages().then(function(res) {
+        var data = res.data;
+        if( res.status === 200) {
+          $scope.landingPages = [];
+          $scope.totalRecords = data.total;
+          data.rows.forEach(function(item) {
+            var path = item.path;
+            item.path = path+'?id=';
+            $scope.landingPages.push(item)
           });
         }
       });
+    }
 
   });
