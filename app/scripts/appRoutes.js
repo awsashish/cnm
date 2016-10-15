bc008b7aca901e15d60581c2225457bdfcd40c4e
Mohammad Raza <mohd.raza@allies.co.in>
2016-10-15 10:06:18 +0530

    OS Ticket plugin enabled

--- app/scripts/appRoutes.js
+++ app/scripts/appRoutes.js
@@ -84,20 +84,20 @@ angular.module('coinomiaFrontendApp')
         controller: 'SuccessCtrl'
     })
 
-    // // Support page
-    // .state('support', {
-    //     url: '/support?return_url',
-    //     templateUrl: 'views/support.html',
-    //     controller: 'SupportCtrl',
-    //     authenticate:true
-    // })
-
     // Support page
     .state('support', {
-        url: '/support',
+        url: '/support?return_url',
         templateUrl: 'views/support.html',
+        controller: 'SupportCtrl',
         authenticate:true
     })
+
+    // Support page
+    // .state('support', {
+    //     url: '/support',
+    //     templateUrl: 'views/support.html',
+    //     authenticate:true
+    // })
 
     // Terms & Conditions page
     .state('terms-and-conditions', {
