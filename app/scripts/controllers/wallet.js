6b38e308ade4b9b0d6680cdb9a13b3839e720d50
Mohammad Raza <mohd.raza@allies.co.in>
2016-10-29 19:17:16 +0530

    Wallet page created

--- app/scripts/controllers/wallet.js
+++ app/scripts/controllers/wallet.js
@@ -8,10 +8,34 @@
  * Controller of the coinomiaFrontendApp
  */
 angular.module('coinomiaFrontendApp')
-  .controller('WalletCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('WalletCtrl', function ($scope, coinomiaService, config) {
+
+    $scope.walletHeading = config.wallet;
+    $scope.currentPage = config.currentPage;
+    $scope.productMaxUnit = config.productMaxUnit;
+    $scope.btcImagePath = config.btcImagePath;
+    $scope.ethImagePath = config.ethImagePath;
+
+
+    // Get Wallet Info
+    $scope.walletInfo = function() {
+      coinomiaService.getWalletInfo()
+        .then(function(res){
+          if(res.status === 200){
+            $scope.walletData = res.data;
+          }
+        });
+        $scope.getTransctions();
+    }
+
+    $scope.getTransctions = function() {
+      coinomiaService.getTransactionDetails($scope.currentPage)
+        .then(function(res){
+          if(res === 200) {
+            $scope.transactionDetails = res.data;
+          }
+        })
+    }
+
+    $scope.walletInfo();
   });
