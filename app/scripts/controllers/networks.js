d49a7a77265cf4a65ca8db03fb1ad6ca9d5f465b
Mohammad Raza <mohd.raza@allies.co.in>
2016-11-12 13:22:19 +0530

    Fixed pagination issue

--- app/scripts/controllers/networks.js
+++ app/scripts/controllers/networks.js
@@ -10,6 +10,8 @@
 angular.module('coinomiaFrontendApp')
   .controller('NetworksCtrl', function ($scope, $rootScope, $timeout, $location, $uibModal, $uibModalStack, $window, $filter, coinomiaService, config, UtilsService) {
     $scope.currentPage = config.currentPage;
+    $scope.teamCurrentPage = config.currentPage;
+    $scope.directsCurrentPage = config.currentPage;
     $scope.loadingDates = true;
     $scope.pagination = {
       totalDirects: 0,
@@ -99,6 +101,7 @@ angular.module('coinomiaFrontendApp')
 
     // Get All Referrals
     $scope.allReferral = function(page) {
+      $scope.teamData = [];
       coinomiaService.getAllReferral(page)
         .then(function(res) {
           if(res.status === 200) {
