a8dbe3806c6596df12ae0347623e66f8b5509c75
Mohammad Raza <mohd.raza@allies.co.in>
2016-12-31 16:11:23 +0530

    Video pop up added

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -8,7 +8,7 @@
  * Controller of the coinomiaFrontendApp
  */
 angular.module('coinomiaFrontendApp')
-  .controller('HeaderCtrl', function ($scope, $cookies, $state, $rootScope, $localStorage, $timeout, coinomiaService, UtilsService, config) {
+  .controller('HeaderCtrl', function ($scope, $cookies, $state, $rootScope, $localStorage, $timeout, $uibModal, $uibModalStack, coinomiaService, UtilsService, config) {
 
       $rootScope.s3Url = config.s3BucketUrl;
 
@@ -105,5 +105,14 @@ angular.module('coinomiaFrontendApp')
         $rootScope.latestSignup = res.data.data;
       }
     })
+
+    // Open Video Modal
+    $scope.openVideo = function() {
+      console.log(1);
+      var modalInstance = $uibModal.open({
+          templateUrl: 'views/modal/academy-video.html',
+          size: 'md'
+      });
+    }
 
   });
