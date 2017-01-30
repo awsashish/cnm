bbc104f1cd2f1cc04fcf865e92470ef27b363db7
Mohammad Raza <mohd.raza@allies.co.in>
2017-01-30 15:45:24 +0530

    Changes reverted

--- app/scripts/services/coinomiaservice.js
+++ app/scripts/services/coinomiaservice.js
@@ -11,6 +11,7 @@ angular.module('coinomiaFrontendApp')
   .service('coinomiaService', function ($http, $log, $state, $window, $cookies, $localStorage, $location, config) {
 
     var pageLimit = config.pageLimit;
+    
     this.apiHost = 'https://api.coinomia.com/';
 
     // if($location.host() === 'login.coinomia.com') {
