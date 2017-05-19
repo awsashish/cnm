aaa6719b920285501591bebd6581b6d275bf4f6d
Sanjay Tripathi <sanjay.tripathi@geminisolutions.in>
2017-05-19 13:08:20 +0530

    Implemented remove from Cart functionality and dynamic design of Coinomia Academy Cart Page

--- app/scripts/controllers/coinomiaacademycart.js
+++ app/scripts/controllers/coinomiaacademycart.js
@@ -1,6 +1,25 @@
 'use strict';
 
-angular.module('coinomiaFrontendApp')
-  .controller('CoinomiaAcademyCartCtrl', function ($scope, $state) {
+angular.module('coinomiaFrontendApp').controller('CoinomiaAcademyCartCtrl', function ($scope, $state, coinomiaService) {
 
-  });
+	$scope.totalPrice = 0;
+  	$scope.getCartItems = function() {
+		coinomiaService.academyPackages()
+		.then(function(result) {
+			if(result.status === 200) {
+				$scope.cartItems = result.data.rows;
+				angular.forEach($scope.cartItems, function(cartItem){
+					$scope.totalPrice += parseInt(cartItem.Price);
+				})
+			}
+		})
+	};
+
+	$scope.removeFromCart = function (item, index){
+		$scope.cartItems.splice(index, 1);
+		$scope.totalPrice = $scope.totalPrice - parseInt(item.Price);
+	}
+
+	$scope.getCartItems();
+
+});
