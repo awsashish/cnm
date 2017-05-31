008eb5924abd62aa6faf26c9a2420c4fe38c0ac1
Sanjay Tripathi <sanjay.tripathi@geminisolutions.in>
2017-05-31 18:03:10 +0530

    Fixed issues

--- app/scripts/controllers/coinomiaacademy.js
+++ app/scripts/controllers/coinomiaacademy.js
@@ -3,20 +3,21 @@
 angular.module('coinomiaFrontendApp')
   .controller('CoinomiaAcademyCtrl', function ($scope, $state, coinomiaService) {
 
-  	$scope.getAcademyPackages = function() {
-		coinomiaService.academyPackages()
-		.then(function(res) {
-			if(res.status === 200) {
-				$scope.packages = res.data.rows;
-			}
-		});
-	};
-
 	$scope.getPackagePurchaseHistory = function () {
 		coinomiaService.packagePurchaseHistory()
 		.then(function(result) {
 			if(result.status === 200) {
 				$scope.packagesBought = result.data;
+				$scope.getAcademyPackages();
+			}
+		});
+	};
+
+	$scope.getAcademyPackages = function() {
+		coinomiaService.academyPackages()
+		.then(function(res) {
+			if(res.status === 200) {
+				$scope.packages = res.data.rows;
 				$scope.getPurchaseHistory();
 			}
 		});
@@ -32,6 +33,5 @@ angular.module('coinomiaFrontendApp')
 		})
 	};
 
-	$scope.getAcademyPackages();
 	$scope.getPackagePurchaseHistory();
   });
